0ff1ae6b7f904e1a04bb7481b514b785136a90ea
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-01-13 11:56:00 +0000

    Added 9 functions

--- Makefile
+++ Makefile
@@ -11,12 +11,22 @@
 # **************************************************************************** #
 
 SRCS	=  ft_strlen.c				\
-				ft_atoi.c			\
-				ft_strdup.c			\
-				ft_strncmp.c		\
 				ft_strlcpy.c		\
 				ft_strlcat.c		\
 				ft_strchr.c			\
+				ft_strrchr.c		\
+				ft_strnstr.c		\
+				ft_strncmp.c		\
+				ft_atoi.c			\
+				ft_isalpha.c		\
+				ft_isdigit.c		\
+				ft_isalnum.c		\
+				ft_isascii.c		\
+				ft_isprint.c		\
+				ft_toupper.c		\
+				ft_tolower.c		\
+				ft_strdup.c			\
+
 
 OBJS	= ${SRCS:.c=.o}
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 09:45:41 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/01/12 19:26:12 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/01/13 11:42:59 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <strings.h>
 # include <unistd.h>
 # include <limits.h>
 # include <stddef.h>
@@ -24,9 +25,19 @@ size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 char		*ft_strchr(const char *s, int c);
-// char		*ft_strrchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_atoi(const char *str);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 char		*ft_strdup(const char *s);
 
 #endif
+
+// #include "libft.h"
